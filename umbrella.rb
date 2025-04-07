6d5f99f993e401efe8a9ddc289c730bde1918312
gbotas <gbotas@uchicago.edu>
2025-04-07 21:46:40 +0000

    current temperature

--- umbrella.rb
+++ umbrella.rb
@@ -7,9 +7,9 @@ pp "Will you need an umbrella today?"
 
 pp "Where are you?"
 
-# user_location = gets.chomp
+user_location = gets.chomp
 
-user_location = "Madrid"
+#user_location = "Madrid"
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
@@ -29,6 +29,24 @@ geometry = results2.fetch("geometry")
   
 location= geometry.fetch("location")
 
-pp latitude = location.fetch("lat")
+pp latitude = location.fetch("lat").to_s
 
-pp longitude = location.fetch("lng")
+pp longitude = location.fetch("lng").to_s
+
+
+#Take the latitude and longitude, assemble the correct URL for the Pirate Weather API, get it, parse it, and dig out current temperature
+
+
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + ENV.fetch("PIRATE_WEATHER_KEY") + "/" + latitude+ "," + longitude
+
+raw_pw_response = HTTP.get(pirate_weather_url)
+
+parsed_pw_response = JSON.parse(raw_pw_response)
+
+# pp parsed_pw_response.keys
+
+currently_hash = parsed_pw_response.fetch("currently")
+
+current_temp = currently_hash.fetch("temperature")
+
+puts "The current temperature is " + current_temp.to_s + "."
